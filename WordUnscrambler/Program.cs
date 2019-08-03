411e998946c6ccceeaacedfc1f547048fe8755c0
Ryan Foster <fosterdavidryan@gmail.com>
2019-08-03 22:47:18 +0100

    instantiate empty list and get list size before and after adding item to it

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -11,6 +11,10 @@ namespace WordUnscrambler
 		static void Main(string[] args)
 		{
 			List<int> listy = new List<int>();
+
+			Console.WriteLine(listy.Count());
+			listy.Add(5);
+			Console.WriteLine(listy.Count());
 		}
 	}
 }
